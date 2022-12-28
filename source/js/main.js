fb0e90aee083932499b4b5af7f812bb6126b3679
zaikaf <zaikafrontend@gmail.com>
2022-12-28 23:06:17 +0300

    KMA

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-   const questList = [...document.querySelectorAll('.quest__question')]
+   const sliderReviews = tns({
+      container: '.reviews__slider',
+      items: 1,
+      autoplay: true,
+      nav: false,
+      arrowKeys: true,
+      mouseDrag: true,
+      autoplayButtonOutput: false,
+      gutter: 10,
+      controlsPosition: 'bottom',
+      controlsContainer: ".reviews__slider__controls",
+      prevButton: '#left',
+      nextButton: '#right',
+      loop: false
+   });
 
+   //question up-down
+   const questList = [...document.querySelectorAll('.quest__question')]
    questList.forEach( question => {
       question.addEventListener('click', (e) => {
          e.preventDefault();
